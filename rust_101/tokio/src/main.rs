2ca3553c852fadd2a153c3ada54336e389dcdff9
Dot8Pixels <napatsriz@gmail.com>
2025-02-18 15:15:24 +0000

    feat: :rocket: Tokio

---
+++ rust_101/tokio/src/main.rs
@@ -0,0 +1,29 @@
+use tokio;
+
+async fn gather_herbs() {
+    println!("Crabby is gathering herbs...");
+}
+
+async fn collect_gold_coins() {
+    println!("Crabby is collecting gold coins...");
+}
+
+async fn fight_monster() {
+    println!("Crabby is fighting the monster!");
+}
+
+#[tokio::main]
+async fn main() {
+    // Spawn all three tasks here using tokio::spawn()
+    // Crabby wants all quests to run at the same time!
+
+    let gather_herbs_task = tokio::spawn(gather_herbs());
+    let collect_gold_coins_task = tokio::spawn(collect_gold_coins());
+    let fight_monster_task = tokio::spawn(fight_monster());
+
+    let _ = tokio::join!(
+        gather_herbs_task,
+        collect_gold_coins_task,
+        fight_monster_task,
+    );
+}
