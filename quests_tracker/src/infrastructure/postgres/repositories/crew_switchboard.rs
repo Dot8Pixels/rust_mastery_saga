619221b245e561c61a646ef7938a4f478a4366ab
Dot8Pixels <napatsriz@gmail.com>
2025-03-07 22:39:09 +0700

    feat: :sparkles: Repositories for Postgres

---
+++ quests_tracker/src/infrastructure/postgres/repositories/crew_switchboard.rs
@@ -0,0 +1,32 @@
+use anyhow::Result;
+use async_trait::async_trait;
+use std::sync::Arc;
+
+use crate::{
+    domain::{
+        repositories::crew_switchboard::CrewSwitchboardRepository,
+        value_objects::quest_adventurer_junction::QuestAdventurerJunction,
+    },
+    infrastructure::postgres::postgres_connection::PgPoolSquad,
+};
+
+pub struct CrewSwitchboardPostgres {
+    db_pool: Arc<PgPoolSquad>,
+}
+
+impl CrewSwitchboardPostgres {
+    pub fn new(db_pool: Arc<PgPoolSquad>) -> Self {
+        Self { db_pool }
+    }
+}
+
+#[async_trait]
+impl CrewSwitchboardRepository for CrewSwitchboardPostgres {
+    async fn join(&self, junction_body: QuestAdventurerJunction) -> Result<()> {
+        unimplemented!()
+    }
+
+    async fn leave(&self, junction_body: QuestAdventurerJunction) -> Result<()> {
+        unimplemented!()
+    }
+}
---
+++ quests_tracker/src/infrastructure/postgres/repositories/guild_commanders.rs
@@ -0,0 +1,34 @@
+use crate::{
+    domain::{
+        entities::guild_commanders::{GuildCommanderEntity, RegisterGuildCommanderEntity},
+        repositories::guild_commanders::GuildCommandersRepository,
+    },
+    infrastructure::postgres::postgres_connection::PgPoolSquad,
+};
+use anyhow::Result;
+use async_trait::async_trait;
+use std::sync::Arc;
+
+pub struct GuildCommandersPostgres {
+    db_pool: Arc<PgPoolSquad>,
+}
+
+impl GuildCommandersPostgres {
+    pub fn new(db_pool: Arc<PgPoolSquad>) -> Self {
+        Self { db_pool }
+    }
+}
+
+#[async_trait]
+impl GuildCommandersRepository for GuildCommandersPostgres {
+    async fn register(
+        &self,
+        register_guild_commander_entity: RegisterGuildCommanderEntity,
+    ) -> Result<i32> {
+        unimplemented!()
+    }
+
+    async fn find_by_username(&self, username: String) -> Result<GuildCommanderEntity> {
+        unimplemented!()
+    }
+}
