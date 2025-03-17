c6419c3b44feda400314256fc2da97b5c572ba2c
Dot8Pixels <napatsriz@gmail.com>
2025-03-17 22:44:41 +0700

    feat: :sparkles: Router

--- quests_tracker/src/infrastructure/axum_http/http_serve.rs
+++ quests_tracker/src/infrastructure/axum_http/http_serve.rs
@@ -1,5 +1,9 @@
 use crate::{
-    config::config_model::DotEnvyConfig, infrastructure::postgres::postgres_connection::PgPoolSquad,
+    config::config_model::DotEnvyConfig,
+    infrastructure::{
+        axum_http::routers::{self, adventures::routes},
+        postgres::postgres_connection::PgPoolSquad,
+    },
 };
 use anyhow::Result;
 use axum::{http::Method, routing::get, Router};
@@ -18,6 +22,34 @@ use super::default_routers;
 pub async fn start(config: Arc<DotEnvyConfig>, db_pool: Arc<PgPoolSquad>) -> Result<()> {
     let app = Router::new()
         .fallback(default_routers::not_found)
+        .nest(
+            "/journey-ledger",
+            routers::journey_ledger::routes(Arc::clone(&db_pool)),
+        )
+        .nest(
+            "/quest-ops",
+            routers::quest_ops::routes(Arc::clone(&db_pool)),
+        )
+        .nest(
+            "/crew-switchboard",
+            routers::crew_switchboard::routes(Arc::clone(&db_pool)),
+        )
+        .nest(
+            "/guild-commanders",
+            routers::guild_commanders::routes(Arc::clone(&db_pool)),
+        )
+        .nest(
+            "/adventurers",
+            routers::adventures::routes(Arc::clone(&db_pool)),
+        )
+        .nest(
+            "/quest-viewing",
+            routers::quest_viewing::routes(Arc::clone(&db_pool)),
+        )
+        .nest(
+            "/authentication",
+            routers::authentication::routes(Arc::clone(&db_pool)),
+        )
         .route("/health-check", get(default_routers::health_check))
         .layer(TimeoutLayer::new(Duration::from_secs(
             config.server.timeout,
